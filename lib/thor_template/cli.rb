49cf7177b098e7213f346934f63dc5ecb793bd2a
Tung Nguyen <tongueroo@gmail.com>
2013-11-30 01:12:38 -0800

    add example of subcommand

--- lib/thor_template/cli.rb
+++ lib/thor_template/cli.rb
@@ -1,11 +1,42 @@
 require 'thor'
 
 module ThorTemplate
-  class CLI < Thor
-    desc 'foo', 'foo'
-    long_desc 'foo'
-    def foo
-      puts "foo"
+
+  class Remote < Thor
+    desc "add <name> <url>", "Adds a remote named <name> for the repository at <url>"
+    long_desc <<-LONGDESC
+      Adds a remote named <name> for the repository at <url>. The command git fetch <name> can then be used to create and update
+    LONGDESC
+    option :t, :banner => "<branch>"
+    option :m, :banner => "<master>"
+    options :f => :boolean, :tags => :boolean, :mirror => :string
+    def add(name, url)
+      # implement git remote add
     end
+ 
+    desc "rename <old> <new>", "Rename the remote named <old> to <new>"
+    def rename(old, new)
+    end
+  end
+
+  class CLI < Thor
+    class_option :verbose, :type => :boolean
+
+    desc "hello NAME", "say hello to NAME"
+    option :from
+    def hello(name)
+      puts "from: #{options[:from]}" if options[:from]
+      puts "Hello #{name}"
+    end
+
+    desc "fetch <repository> [<refspec>...]", "Download objects and refs from another repository"
+    options :all => :boolean, :multiple => :boolean
+    option :append, :type => :boolean, :aliases => :a
+    def fetch(respository, *refspec)
+      # implement git fetch here
+    end
+ 
+    desc "remote SUBCOMMAND ...ARGS", "manage set of tracked repositories"
+    subcommand "remote", Remote
   end
 end
