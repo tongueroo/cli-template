6bf6d36f83a7fd8fc01ee51e75c3da454a1b8f52
Tung Nguyen <tongueroo@gmail.com>
2014-03-27 16:48:35 -0700

    add lib to load_path instead

--- lib/thor_template.rb
+++ lib/thor_template.rb
@@ -1,6 +1,7 @@
-$:.unshift(File.expand_path("../thor_template", __FILE__))
+$:.unshift(File.expand_path("../", __FILE__))
 require "version"
 require "thor/vcr" if ENV['VCR'] == '1'
+require "thor_template/cli"
 
 module ThorTemplate
   # Your code goes here...
