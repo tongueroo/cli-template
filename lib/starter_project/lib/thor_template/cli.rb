8392da5edd9b04987c63a0b7810986874eca9aa0
Tung Nguyen <tongueroo@gmail.com>
2016-09-28 10:47:41 -0700

    Update starter project to use ruby 2 syntax

--- lib/starter_project/lib/thor_template/cli.rb
+++ lib/starter_project/lib/thor_template/cli.rb
@@ -4,12 +4,12 @@ require 'thor_template/cli/help'
 module ThorTemplate
 
   class CLI < Thor
-    class_option :verbose, :type => :boolean
-    class_option :noop, :type => :boolean
+    class_option :verbose, type:  :boolean
+    class_option :noop, type:  :boolean
 
     desc "hello NAME", "say hello to NAME"
     long_desc Help.hello
-    option :from, :desc => 'from person'
+    option :from, desc: 'from person'
     def hello(name)
       puts "from: #{options[:from]}" if options[:from]
       puts "Hello #{name}"
