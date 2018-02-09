d4772a2e3b72c04e555f2315b62ea22f37ec13a3
Tung Nguyen <tongueroo@gmail.com>
2018-02-09 02:24:44 -0800

    fix dash name issue, use underscore for paths

--- lib/cli-template/helpers.rb
+++ lib/cli-template/helpers.rb
@@ -1,6 +1,12 @@
 module CliTemplate::Helpers
-  # helper variables
   def project_class_name
-    project_name.camelize
+    project_name.underscore.camelize
+  end
+
+  # Files should be named with underscores instead of dashes even
+  # though project name can contain a dash. This is because autoloading
+  # works will underscores in the filenames only.
+  def underscored_name
+    project_name.underscore
   end
 end
