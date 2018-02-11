056894fce99bd8de48f9220a3acff40b687c0470
Tung Nguyen <tongueroo@gmail.com>
2018-02-10 19:54:25 -0800

    rename to colon_namespaces and run specs for both templates

--- spec/lib/cli_spec.rb
+++ spec/lib/cli_spec.rb
@@ -12,49 +12,50 @@ require 'spec_helper'
 #
 # Because it shells out tests take about 20 seconds to run this spec file.
 describe CliTemplate::CLI do
-  before(:all) do
-    @args = "--noop"
-  end
+  templates = %w[default colon_namespaces]
+  templates.each do |template|
+    context("template #{template}") do
+      describe "cli-template new" do
+        context("simple single name") do
+          it "should generate" do
+            out = execute("cd tmp && ../exe/cli-template new hello #{@args}")
+            expect(out).to include("Creating new project called hello")
+            expect(out).to include("You have successfully created a CLI project")
+            out = execute("cd tmp/hello && rake")
+            expect(out).to include("0 failures")
+          end
+        end
 
-  describe "new" do
-    context("simple single name") do
-      it "should generate" do
-        out = execute("cd tmp && ../exe/cli-template new hello #{@args}")
-        expect(out).to include("Creating new project called hello")
-        expect(out).to include("You have successfully created a CLI project")
-        out = execute("cd tmp/hello && rake")
-        expect(out).to include("0 failures")
-      end
-    end
+        context("underscored name") do
+          it "should generate" do
+            out = execute("cd tmp && ../exe/cli-template new my_cli #{@args}")
+            expect(out).to include("Creating new project called my_cli")
+            expect(out).to include("You have successfully created a CLI project")
+            out = execute("cd tmp/my_cli && rake")
+            expect(out).to include("0 failures")
+          end
+        end
 
-    context("underscored name") do
-      it "should generate" do
-        out = execute("cd tmp && ../exe/cli-template new my_cli #{@args}")
-        expect(out).to include("Creating new project called my_cli")
-        expect(out).to include("You have successfully created a CLI project")
-        out = execute("cd tmp/my_cli && rake")
-        expect(out).to include("0 failures")
-      end
-    end
+        context("dasherized name") do
+          it "should generate" do
+            out = execute("cd tmp && ../exe/cli-template new my-cli #{@args}")
+            expect(out).to include("Creating new project called my-cli")
+            expect(out).to include("You have successfully created a CLI project")
+            out = execute("cd tmp/my-cli && rake")
+            expect(out).to include("0 failures")
+          end
+        end
 
-    context("dasherized name") do
-      it "should generate" do
-        out = execute("cd tmp && ../exe/cli-template new my-cli #{@args}")
-        expect(out).to include("Creating new project called my-cli")
-        expect(out).to include("You have successfully created a CLI project")
-        out = execute("cd tmp/my-cli && rake")
-        expect(out).to include("0 failures")
-      end
-    end
-
-    # CamelCase is ugly :(
-    context("simple CamelCase name") do
-      it "should generate" do
-        out = execute("cd tmp && ../exe/cli-template new MyCli #{@args}")
-        expect(out).to include("Creating new project called MyCli")
-        expect(out).to include("You have successfully created a CLI project")
-        out = execute("cd tmp/MyCli && rake")
-        expect(out).to include("0 failures")
+        # CamelCase is ugly :(
+        context("simple CamelCase name") do
+          it "should generate" do
+            out = execute("cd tmp && ../exe/cli-template new MyCli #{@args}")
+            expect(out).to include("Creating new project called MyCli")
+            expect(out).to include("You have successfully created a CLI project")
+            out = execute("cd tmp/MyCli && rake")
+            expect(out).to include("0 failures")
+          end
+        end
       end
     end
   end
